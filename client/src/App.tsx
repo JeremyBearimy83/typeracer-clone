c2e494bb78aa0b23c5c2b6531150095928dfff8c
Akhil Kala <akhilkala@pop-os.localdomain>
2020-12-25 22:31:53 +0530

    boilerplate change

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,10 @@
 import React, { ReactElement } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-import GlobalStyles from "./GlobalStyles";
-import Theme from "./Theme";
+import { Switch, Route } from "react-router-dom";
 
 export default function App(): ReactElement {
   return (
     <React.Fragment>
-      <GlobalStyles />
-      <Router>
-        <Switch>
-          <Theme>{/* <Route path="/" exact component={} /> */}</Theme>
-        </Switch>
-      </Router>
+      <Switch>{/* <Route path="/" component={} /> */}</Switch>
     </React.Fragment>
   );
 }
--- client/src/components/pages/Home.tsx
+++
@@ -1,8 +0,0 @@
-import React, { ReactElement } from "react";
-import styled from "styled-components";
-
-const HomeDiv = styled.div``;
-
-export default function Home(): ReactElement {
-  return <HomeDiv></HomeDiv>;
-}

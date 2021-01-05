6a58e46a0c2d82c0804a66cf970288e042ce0099
Akhil Kala <akhilkala@pop-os.localdomain>
2021-01-05 11:14:33 +0530

    init

---
+++ api/example.js
@@ -0,0 +1,166 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList,
+  GraphQLInt,
+  GraphQLNonNull,
+} = require("graphql");
+const dotenv = require("dotenv");
+const { type } = require("os");
+
+const app = express();
+dotenv.config();
+
+const authors = [
+  { id: 1, name: "J. K. Rowling" },
+  { id: 2, name: "J. R. R. Tolkien" },
+  { id: 3, name: "Brent Weeks" },
+];
+
+const books = [
+  { id: 1, name: "Harry Potter and the Chamber of Secrets", authorId: 1 },
+  { id: 2, name: "Harry Potter and the Prisoner of Azkaban", authorId: 1 },
+  { id: 3, name: "Harry Potter and the Goblet of Fire", authorId: 1 },
+  { id: 4, name: "The Fellowship of the Ring", authorId: 2 },
+  { id: 5, name: "The Two Towers", authorId: 2 },
+  { id: 6, name: "The Return of the King", authorId: 2 },
+  { id: 7, name: "The Way of Shadows", authorId: 3 },
+  { id: 8, name: "Beyond the Shadows", authorId: 3 },
+];
+
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  description: "Author",
+  fields: () => ({
+    id: { type: GraphQLNonNull(GraphQLInt) },
+    name: { type: GraphQLNonNull(GraphQLString) },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve: (author) => {
+        return books.filter((book) => book.authorId === author.id);
+      },
+    },
+  }),
+});
+
+const BookType = new GraphQLObjectType({
+  name: "Book",
+  description: "Book",
+  fields: () => ({
+    id: {
+      type: GraphQLNonNull(GraphQLInt),
+    },
+    name: {
+      type: GraphQLNonNull(GraphQLString),
+    },
+    authorId: { type: GraphQLNonNull(GraphQLInt) },
+
+    author: {
+      type: AuthorType,
+      resolve: (book) => authors.find((author) => author.id === book.authorId),
+    },
+  }),
+});
+
+const RootQueryType = new GraphQLObjectType({
+  name: "query",
+  description: "root",
+  fields: () => ({
+    books: {
+      type: GraphQLList(BookType),
+      description: "List of Books",
+      resolve: () => books,
+    },
+    authors: {
+      type: GraphQLList(AuthorType),
+      description: "List of Authors",
+      resolve: () => authors,
+    },
+    book: {
+      type: BookType,
+      description: "Book",
+      args: {
+        id: {
+          type: GraphQLNonNull(GraphQLInt),
+        },
+      },
+      resolve: (parent, args) => books.find((books) => books.id === args.id),
+    },
+    author: {
+      type: AuthorType,
+      description: "Authors",
+      args: {
+        id: {
+          type: GraphQLNonNull(GraphQLInt),
+        },
+      },
+      resolve: (parent, args) =>
+        authors.find((author) => author.id === args.id),
+    },
+  }),
+});
+
+const RootMutationType = new GraphQLObjectType({
+  name: "Mutation",
+  description: "Root Mutation",
+  fields: () => ({
+    addBook: {
+      type: BookType,
+      description: "Add Book",
+      args: {
+        name: {
+          type: GraphQLNonNull(GraphQLString),
+        },
+        authorId: {
+          type: GraphQLNonNull(GraphQLInt),
+        },
+      },
+      resolve: (parent, args) => {
+        const book = {
+          id: books.length + 1,
+          name: args.name,
+          authorId: args.authorId,
+        };
+        books.push(book);
+        return book;
+      },
+    },
+    addAuthor: {
+      type: AuthorType,
+      description: "Add Author",
+      args: {
+        name: {
+          type: GraphQLNonNull(GraphQLString),
+        },
+      },
+      resolve: (parent, args) => {
+        const author = {
+          id: authors.length + 1,
+          name: args.name,
+        };
+        authors.push(author);
+        return author;
+      },
+    },
+  }),
+});
+
+const schema = new GraphQLSchema({
+  query: RootQueryType,
+  mutation: RootMutationType,
+});
+
+app.use(
+  "/",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server Running on port ${process.env.PORT}...`);
+});
--- client/src/components/pages/Room.tsx
+++
@@ -1,5 +0,0 @@
-import React, { ReactElement } from "react";
-
-export default function Room(): ReactElement {
-  return <div></div>;
-}
